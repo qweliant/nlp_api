507954469b19e4167963e49d8e604f49dfad92f4
qweliant <qweliantanner@gmail.com>
2020-06-06 11:00:46 -0400

    added route

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/nlp/bert")
 async def root():
-    return {"message": "Hello World"}
+    return {"bert": "Hello Foucault"}
+
+@app.get("/nlp/openaigpt")
+async def root():
+    return {"message": "Hello Gyyatri"}
