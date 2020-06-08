c6866555444ec3fbb5bb88387ac91498b12a40be
qweliant <qweliantanner@gmail.com>
2020-06-07 22:14:24 -0400

    mvp

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from nlp import NLP
 
+nlp = NLP()
 app = FastAPI()
+
+
 
 
 @app.get("/nlp/foucault")
 async def root():
-    return {"bert": "Hello Foucault"}
+    TRAIN_TEXT = """The work of an intellectual is not to mould the political will of others; it is, through the analyses that he does in his own field, to re-examine evidence and assumptions, to shake up habitual ways of working and thinking, to dissipate conventional familiarities, to re-evaluate rules and institutions and to participate in the formation of a political will (where he has his role as citizen to play)."""
+    prompt = "Until this is user text"
+    return {"Foucault": nlp.generate(TRAIN_TEXT, prompt)}
 
 
-@app.get("/nlp/Gyyatri")
+@app.get("/nlp/gyyatri")
 async def root():
-    return {"message": "Hello Gyyatri"}
+    TRAIN_TEXT = """In this era of global capital triumphant, to keep responsibility alive in the reading and teaching of the textual is at first sight impractical. It is, however, the right of the textual to be so responsible, responsive, answerable. The “planet” is, here, as perhaps always, a catachresis for inscribing collective responsibility as right. Its alterity, determining experience, is mysterious and discontinuous—an experience of the impossible. It is such collectivities that must be opened up with the question “How many are we?” when cultural origin is detranscendentalized into fiction—the toughest task in the diaspora."""
+    prompt = nlp.summarize()
+    return {"Gyyatri": nlp.generate(TRAIN_TEXT,prompt)}
---
+++ nlp.py
@@ -0,0 +1,41 @@
+from transformers import (
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+    AutoModelWithLMHead,
+    AutoTokenizer,
+    pipeline,
+)
+
+import re
+
+
+class NLP:
+    def __init__(self):
+        self.model = GPT2LMHeadModel.from_pretrained("gpt2")
+        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        self.generated = None
+
+    def generate(self, TRAIN_TEXT, prompt):
+        inputs = self.tokenizer.encode(
+            TRAIN_TEXT + prompt, add_special_tokens=False, return_tensors="pt"
+        )
+
+        prompt_length = len(
+            self.tokenizer.decode(
+                inputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+        )
+        outputs = self.model.generate(
+            inputs, max_length=250, do_sample=True, top_p=0.95, top_k=60
+        )
+        self.generated = str(prompt + self.tokenizer.decode(outputs[0])[prompt_length:])
+        re.sub("[^A-Za-z0-9]+", "", self.generated)
+        return self.generated
+
+    def summarize(self):
+
+        summarizer = pipeline("summarization")
+        # print(self.generated, "\n#######HEY ARE YOU DOING WANT TO PRINT TEXT########")
+        v = summarizer(self.generated, max_length=250, min_length=30)[0]["summary_text"]
+
+        return v
