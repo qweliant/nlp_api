f292872006dd57551d9070f6ce243162914b2e4d
qweliant <qweliantanner@gmail.com>
2020-06-25 21:56:41 -0400

    push changes

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = FastAPI()
 async def root():
     TRAIN_TEXT = """The work of an intellectual is not to mould the political will of others; it is, through the analyses that he does in his own field, to re-examine evidence and assumptions, to shake up habitual ways of working and thinking, to dissipate conventional familiarities, to re-evaluate rules and institutions and to participate in the formation of a political will (where he has his role as citizen to play)."""
     prompt = nlp.summarize()
-    return {"Foucault": nlp.generate(TRAIN_TEXT, prompt)}
+    return {"Foucault": nlp.generate(prompt,TRAIN_TEXT)}
 
 
 @app.get("/nlp/gyyatri")
 async def root_2():
     TRAIN_TEXT = """In this era of global capital triumphant, to keep responsibility alive in the reading and teaching of the textual is at first sight impractical. It is, however, the right of the textual to be so responsible, responsive, answerable. The “planet” is, here, as perhaps always, a catachresis for inscribing collective responsibility as right. Its alterity, determining experience, is mysterious and discontinuous—an experience of the impossible. It is such collectivities that must be opened up with the question “How many are we?” when cultural origin is detranscendentalized into fiction—the toughest task in the diaspora."""
     prompt = nlp.summarize()
-    return {"Gyyatri": nlp.generate(TRAIN_TEXT, prompt)}
+    return {"Gyyatri": nlp.generate(prompt, TRAIN_TEXT)}
--- nlp.py
+++ nlp.py
@@ -6,17 +6,7 @@ from transformers import (
 
 import re
 
-sting  = " One day I said to my self in my own thought ‘whom am I praying to or is  there  a  God  who  listens  to  me?’  At  this  thought  I  was  invaded  by  dead full sadness and I said: \
-‘In vain have I kept my own heart pure (as David says). Later on I thought of the words of the same David, ‘Is the  inventor  of  the  ear  unable  to  hear?’ \
-and  I  said:  ‘who  is  it  thatprovided  me  with  an  ear  to  hear,  who  created  me  as  a  rational[being] and how have I come into this world? \
-Where do I come from? Had  I  lived  before  the  creator  of  the  world,  I  would  have  known  the  beginning of my life and of the consciousness [of myself] \
-that created me? Was I created by my own hands? But I didn’t exist before I was created. If I say that my father and my mother created me, \
-then I must search for the creator of my parents and of the parents of my parents until they arrive at the first who were not created as we [are] \
-but who came into this world in some other way without being generated. For if  they  themselves  have  been  created,  I  know  nothing  of  their  origin  unless I say, ‘he who created them from nothing most be an uncreated 2 \
-essence  who  is  and  will  be  for  all  centuries  [to  come]  the  lord  and  master  of  all  things,  without  beginning  or  end,  immutable,  whose  years cannot be numbered.’ \
-And I said: ‘Therefore, there is a creator; else there would have been no creation. This creator who endowed us with the gifts of intelligence and reason, \
-cannot he himself be without them?  For  he  created  us  as  intelligent  beings  from  the  abundance  of  this intelligence and the same one being comprehends all, \
-creates all,is  almighty.’  And  I  used  to  say:  ‘my  creator  will  hear  me  if  I  pray  to  him,’  and  because  of  this  thought  I  felt  very  happy"
+sting  = " One day I said to my self in my own thought ‘whom am I praying to or is  there  a  God  who  listens  to  me?’  At  this  thought  I  was  invaded  by  dead full sadness"
 
 class NLP:
     def __init__(self):
@@ -36,7 +26,7 @@ class NLP:
             )
         )
         outputs = self.model.generate(
-            inputs, max_length=800, do_sample=True, top_p=0.95, top_k=111
+            inputs, max_length=225, do_sample=True, top_p=0.95, top_k=111
         )
         self.generated = str(prompt + self.tokenizer.decode(outputs[0])[prompt_length:])
         self.generated = re.sub("[^A-Za-z0-9 ]+", "", self.generated)
@@ -44,14 +34,13 @@ class NLP:
 
     def summarize(self, text=sting):
 
-        if self.generated:
+        if self.generated is None:
             summarizer = pipeline("summarization")
-            v = summarizer(self.generated, max_length=250, min_length=30)[0]["summary_text"]
+            v = summarizer(text, max_length=80, min_length=30)[0]["summary_text"]
             return v
         
         summarizer = pipeline("summarization")
-        v = summarizer(text, max_length=250, min_length=30)[0]["summary_text"]
-
+        v = summarizer(self.generated, max_length=80, min_length=30)[0]["summary_text"]
         return v
 
     def save_model(self):
